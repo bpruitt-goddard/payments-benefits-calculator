550429591ae0413ce633931f2973df84191b5cf7
Brian Pruitt-Goddard <bpruitt-goddard@github>
2023-11-29 09:59:35 -0800

    feat: implement get all dependents api

--- PaylocityBenefitsCalculator/Api/Controllers/DependentsController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/DependentsController.cs
@@ -1,6 +1,8 @@
-﻿using Api.Dtos.Dependent;
+﻿using Api.Data;
+using Api.Dtos.Dependent;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace Api.Controllers;
@@ -9,6 +11,13 @@ namespace Api.Controllers;
 [Route("api/v1/[controller]")]
 public class DependentsController : ControllerBase
 {
+    private readonly EmployeeDbContext _context;
+
+    public DependentsController(EmployeeDbContext context)
+    {
+        _context = context;
+    }
+
     [SwaggerOperation(Summary = "Get dependent by id")]
     [HttpGet("{id}")]
     public async Task<ActionResult<ApiResponse<GetDependentDto>>> Get(int id)
@@ -20,6 +29,16 @@ public class DependentsController : ControllerBase
     [HttpGet("")]
     public async Task<ActionResult<ApiResponse<List<GetDependentDto>>>> GetAll()
     {
-        throw new NotImplementedException();
+        var dependents = _context.Employees
+            .Include(e => e.Dependents)
+            .SelectMany(e => e.Dependents)
+            .Select(d => new GetDependentDto(d))
+            .ToList();
+
+        return new ApiResponse<List<GetDependentDto>>
+        {
+            Data = dependents,
+            Success = true
+        };
     }
 }
